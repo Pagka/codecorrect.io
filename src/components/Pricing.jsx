4fc4d94ee7f509a3cd312a7096f5714d71adee82
Kamfeskaya <tatendachris@gmail.com>
2022-07-04 16:22:42 +0200

    change plan details

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -116,7 +116,7 @@ export function Pricing() {
         <div className="-mx-4 mt-16 grid max-w-2xl grid-cols-1 gap-y-10 sm:mx-auto lg:-mx-8 lg:max-w-none lg:grid-cols-3 xl:mx-0 xl:gap-x-8">
           <Plan
             name="Starter"
-            price="$9"
+            price="$19"
             description="Good for anyone who is self-employed and just getting started."
             href="/register"
             features={[
@@ -130,7 +130,7 @@ export function Pricing() {
           <Plan
             featured
             name="Small business"
-            price="$15"
+            price="$45"
             description="Perfect for small / medium sized businesses."
             href="/register"
             features={[
@@ -145,7 +145,7 @@ export function Pricing() {
           />
           <Plan
             name="Enterprise"
-            price="$39"
+            price="$99"
             description="For even the biggest enterpise companies."
             href="/register"
             features={[
